ab6310c9398f0501e52bfd32912ecd28cad88835
Polly Liu <polly.liu4@gmail.com>
2023-01-21 02:49:37 -0500

    update

--- client/src/components/Jumbotron.tsx
+++ client/src/components/Jumbotron.tsx
@@ -13,7 +13,13 @@ function Jumbotron({ user }) {
             <div className="bg-[url('../../public/jumbotron-gradient.png')]">
             <div className="max-w-screen-lg bg-white shadow-2xl rounded-lg mx-auto text-center py-12">
                 <h2 className="text-3xl leading-9 font-bold tracking-tight text-gray-800 sm:text-4xl sm:leading-10">
-                    Start studying with us
+                <span className="font-extrabold text-grey-800 text-3xl">
+                  Empowr
+                </span>
+                <span className="font-extrabold text-3xl text-violet-500">
+                  HER
+                </span>
+                
                 </h2>
                 <div className="mt-8 flex justify-center">
                     <div className="inline-flex rounded-md bg-blue-500 shadow">
