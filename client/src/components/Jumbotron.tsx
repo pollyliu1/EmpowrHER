6fafe5a73b1a8f561df96a279dccafad56792813
ricsign <ricsign123@gmail.com>
2023-01-21 02:06:47 -0500

    Update Jumbotron.tsx

--- client/src/components/Jumbotron.tsx
+++ client/src/components/Jumbotron.tsx
@@ -5,22 +5,22 @@ import SignInButton1 from './SignInButton1'
 import SignOutButton1 from './SignOutButton1'
 
 function Jumbotron({ user }) {
-  return (
-    <>
-        <Navbar user={user} />
+    return (
+        <>
+            <Navbar user={user} />
 
 
-        <div className='bg-purple-200'>
-        {user ? 
-        (<>
-        {user.displayName}
-        </> ) : 
-        <SignInButton1 /> }
-        
-        <h1>hi</h1>
-        </div>
-    </>
-  )
+            <div className='bg-purple-200'>
+                {user ?
+                    (<>
+                        {user.displayName}
+                    </>) :
+                    <SignInButton1 />}
+
+                <h1>hi</h1>
+            </div>
+        </>
+    )
 }
 
 export default Jumbotron
