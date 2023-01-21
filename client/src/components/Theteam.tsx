7e921625c25b76b2fab298c9ee83ac6917a2421f
Angela Xu <angela.xu.dev@gmail.com>
2023-01-21 16:31:12 -0500

    Update Theteam.tsx

--- client/src/components/Theteam.tsx
+++ client/src/components/Theteam.tsx
@@ -8,7 +8,7 @@ import SignOutButton1 from "./SignOutButton1";
 function Theteam() {
   return (
     <>
-      <section >
+      <section id="the-team">
         <h1 className="text-center text-3xl">The Team</h1>
         <div className="grid grid-cols-2 gap-4">
           <div className="col-span-1 mx-20 flex bg-indigo-200 shadow-2xl rounded-3xl float-left mt-10 pt-100 py-10 px-10">
