f439189a93c1cfdc85ebedbeef482db1b97f03f0
Angela Xu <angela.xu.dev@gmail.com>
2023-01-21 13:31:28 -0500

    Update home page

--- client/src/components/Jumbotron.tsx
+++ client/src/components/Jumbotron.tsx
@@ -7,6 +7,9 @@ import SignOutButton1 from "./SignOutButton1";
 import separator from "../resources/separator.png";
 import light_gradient from "../resources/light_gradient.png";
 import light_gradient_upsidedown from "../resources/ligth_gradient_upsidedown.png";
+import infopic1 from "../resources/info1.jpg";
+import infopic2 from "../resources/info2.jpg";
+import infopic3 from "../resources/info3.jpg";
 import Image from "next/image";
 
 function Jumbotron({ user }) {
@@ -15,7 +18,7 @@ function Jumbotron({ user }) {
       <div className="bg-[url('../../public/jumbotron-gradient.png')] bg-cover h-full">
         <Navbar user={user} />
         <div className="justify-content h-screen">
-          <section className="pb-100">
+          <section>
             <div className="max-w-screen-lg bg-white shadow-2xl rounded-3xl mx-auto text-center py-12 mt-20 mb-100">
               <h2 className="text-3xl leading-9 font-bold tracking-tight text-gray-800 sm:text-4xl sm:leading-10">
                 <span className="font-extrabold text-gray-800 text-3xl leading-9 tracking-tight sm:text-4xl sm:leading-10">
@@ -33,14 +36,14 @@ function Jumbotron({ user }) {
             </div>
           </section>
 
-          <section style={{ padding: 500 }}>
+          <section style={{ padding: 432 }}>
             <div>
               <Image
                 src={separator}
                 alt="separator bar"
                 style={{
                   position: "absolute",
-                  top: 500,
+                  top: 550,
                   left: 0,
                   right: 0,
                   bottom: 0,
@@ -52,8 +55,43 @@ function Jumbotron({ user }) {
             </div>
           </section>
 
+          <section>
+            <div className="flex">
+              <div className="mx-auto">
+                <Image
+                  src={infopic3}
+                  alt="info picture 3"
+                  style={{ width: 300, height: 300 }}
+                />
+                <p className="text-center text-xl mx-auto font-extrabold text-violet-500">
+                  Teach
+                </p>
+              </div>
+              <div className="mx-auto">
+                <Image
+                  src={infopic2}
+                  alt="info picture 2"
+                  style={{ width: 300, height: 300 }}
+                />
+                <p className="text-center text-xl mx-auto font-extrabold text-emerald-300">
+                  Support
+                </p>
+              </div>
+              <div className="mx-auto">
+                <Image
+                  src={infopic1}
+                  alt="info picture 1"
+                  style={{ width: 300, height: 300 }}
+                />
+                <p className="text-center text-xl mx-auto font-extrabold text-indigo-500">
+                  Build Community
+                </p>
+              </div>
+            </div>
+          </section>
+
           <section style={{ padding: 100 }}>
-            <div className="max-w-2xl bg-indigo-200 shadow-2xl rounded-3xl mx-auto py-10 px-10 md:w-full">
+            <div className="max-w-2xl bg-indigo-200 shadow-2xl rounded-3xl mx-auto pt-100 py-10 px-10 md:w-full">
               <p>
                 "
                 <span className="font-bold">
