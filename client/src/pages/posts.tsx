302d55312ef2a2ba793b4ffc3bc792b792274945
Polly Liu <polly.liu4@gmail.com>
2023-01-21 15:33:57 -0500

    Updated post cards

--- client/src/pages/posts.tsx
+++ client/src/pages/posts.tsx
@@ -8,17 +8,17 @@ import Navbar from "@/components/Navbar";
 import Head from "next/head";
 
 const fetchPosts = async () => {
-  const data = await getDocs(collection(db, "posts"));
-  return data.docs.map((doc: any) => doc.data());
+    const data = await getDocs(collection(db, "posts"));
+    return data.docs.map((doc: any) => doc.data());
 };
 
 export const getServerSideProps = async () => {
-  const posts = await fetchPosts();
-  return {
-    props: {
-      posts,
-    },
-  };
+    const posts = await fetchPosts();
+    return {
+        props: {
+            posts,
+        },
+    };
 };
 
 function Posts({ posts }) {
@@ -35,10 +35,10 @@ function Posts({ posts }) {
 
   return (
     <>
-    <div className="bg-[url('../../public/jumbotron-gradient.png')] bg-cover h-full">
+    <div className="bg-[url('../../public/jumbotron-gradient.png')] bg-cover min-h-screen">
       <Heading />
       <Navbar user={user} />
-      <div className='max-w-lg mx-auto mt-20 border-purple-800'>
+      <div className='max-w-lg mx-auto mt-10 mb-10 border-purple-800'>
         <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden ">
           <div className="grid place-items-center h-full w-12 text-gray-300">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -52,26 +52,46 @@ function Posts({ posts }) {
             placeholder="Search for a skill.." /> 
         </div>
       </div>
-      <div>Posts</div>
-      {/* {posts[0]["description"]} */}
-      {posts.length > 0 &&
-        posts.map((post: any, ind: number) => (
-          <div className="container" key={ind}>
-            <h1>
-              #{post["pid"]} {post["title"]}
-            </h1>
-            <p>Skills Possessed: {post["skills_have"]}</p>
-            <p>Skills Wanted: {post["skills_want"]}</p>
-            <p>Requester: {post["uemail"]}</p>
-            <p>{post["description"]}</p>
-            <p>
-              {post["fulfill_uemail"] == "false"
-                ? "Await empowerment"
-                : "Empowered by " + post["fulfill_uemail"]}
-            </p>
-          </div>
-        ))}
+        {/* {posts[0]["description"]} */}
+        <div className="flex padding-20px">
+            {posts.length > 0 &&
+                posts.map((post: any, ind: number) => (
+                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                        <div className="container" key={ind}>
+                            <img className="rounded-t-lg" src="favicon.ico" alt="" />
+                            <div className="p-5">
+                                <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    {post["pid"]} {post["title"]}
+                                </h1>
+                                <button className="mb-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                                    Skills Possessed
+                                </button>
+                                <span className="inline mb-3 font-normal text-gray-700 dark:text-gray-400 pl-4px">
+                                    &nbsp;{post["skills_have"]}
+                                </span>
+                                <button className="mb-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                                    Skills Wanted
+                                </button>
+                                <span className="inline mb-3 font-normal text-gray-700 dark:text-gray-400 pl-4px">
+                                    &nbsp;{post["skills_want"]}
+                                </span>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                    &nbsp;Requester: {post["uemail"]}
+                                </p>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                    {post["description"]}
+                                </p>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                    {post["fulfill_uemail"] == "false"
+                                        ? "Await empowerment"
+                                        : "Empowered by " + post["fulfill_uemail"]}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
         </div>
+    </div>
     </>
   );
 }
