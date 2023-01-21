6fe59cb817866966d350933cb8dfc38eae6866f3
ricsign <ricsign123@gmail.com>
2023-01-20 23:55:57 -0500

    creating jumbotron component

---
+++ client/src/components/Jumbotron.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Jumbotron() {
+  return (
+    <>
+        <button className="bg-indigo-500 text-white text-lg px-6 py-3 rounded-lg hover:bg-indigo-600">Button</button>
+    </>
+  )
+}
+
+export default Jumbotron
