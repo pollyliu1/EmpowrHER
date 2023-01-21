3b4019621fe461888548a41de8fa9c1709c13e1e
Polly Liu <polly.liu4@gmail.com>
2023-01-21 02:35:33 -0500

    made jumbo container

--- client/src/components/Jumbotron.tsx
+++ client/src/components/Jumbotron.tsx
@@ -10,13 +10,24 @@ function Jumbotron({ user }) {
         <>
             <Navbar user={user} />
 
-
-            <div className="bg-[url('../../public/jumbotron-gradient.jpg')]">
-                {user ?
-                    (<>
-                        {user.displayName}
-                    </>) :
-                    <SignInButton1 />}
+            <div className="bg-[url('../../public/jumbotron-gradient.png')]">
+            <div className="max-w-screen-lg bg-white shadow-2xl rounded-lg mx-auto text-center py-12">
+                <h2 className="text-3xl leading-9 font-bold tracking-tight text-gray-800 sm:text-4xl sm:leading-10">
+                    Start studying with us
+                </h2>
+                <div className="mt-8 flex justify-center">
+                    <div className="inline-flex rounded-md bg-blue-500 shadow">
+                        <a href="#" className="text-gray-200 font-bold py-2 px-6">
+                            Start
+                        </a>
+                    </div>
+                </div>
+            </div>
+                <div>
+                    { user
+                    ? (<> {user.displayName} </>)
+                    : <SignInButton1 /> }
+                </div>
 
                 <h1>hi</h1>
             </div>
