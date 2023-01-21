1771489538cc594ee10135d0ee39d59acb3f6f2c
mkjshen <118473721+mkjshen@users.noreply.github.com>
2023-01-21 00:28:16 -0500

    added navbar

--- client/src/components/Jumbotron.tsx
+++ client/src/components/Jumbotron.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 function Jumbotron() {
   return (
     <>
+        <Navbar />
         <button className="bg-indigo-500 text-white text-lg px-6 py-3 rounded-lg hover:bg-indigo-600">Button</button>
     </>
   )
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className='bg-gray-100'>
+            <div className='max-w-7xl mx-auto border border-red-400'>
+                <div>logo</div>
+
+                <div>primary nav</div>
+
+                <div>secondary nav</div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
