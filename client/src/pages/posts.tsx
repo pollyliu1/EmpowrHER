8d51e3d0379bba44d31757f786156bb69e30118e
Angela Xu <angela.xu.dev@gmail.com>
2023-01-21 12:08:28 -0500

    home page

--- client/src/pages/posts.tsx
+++ client/src/pages/posts.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 import { auth } from "../serve/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
-import Heading from '@/components/Heading';
-import Navbar from '@/components/Navbar'
-import Head from 'next/head';
+import Heading from "@/components/Heading";
+import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 function Posts() {
-    const [user, loading, error] = useAuthState(auth);
+  const [user, loading, error] = useAuthState(auth);
 
-    return (
-        <>
-            <Heading />
-            <Navbar user={user} />
-            <div>Posts</div>
-        </>
-    )
+  return (
+    <>
+      <Heading />
+      <Navbar user={user} />
+      <div>Posts</div>
+    </>
+  );
 }
 
-export default Posts
+export default Posts;
