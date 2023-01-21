9175e8bc01a2c3018faf396b99b3717e74d3e93d
Angela Xu <angela.xu.dev@gmail.com>
2023-01-21 02:25:46 -0500

    duo Color of title

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,35 +1,63 @@
-import React from 'react'
+import React from "react";
 
-import SignInButton2 from './SignInButton2'
-import SignOutButton2 from './SignOutButton2'
+import SignInButton2 from "./SignInButton2";
+import SignOutButton2 from "./SignOutButton2";
 
 function Navbar({ user }) {
-    return (
-        <nav className='bg-gradient-to-r from-violet-200 to-violet-100'>
-            <div className='max-w-7xl mx-auto'>
-                <div className="flex justify-between">
-                    <div className='flex space-x-4'>
-                        <div>
-                            <a href='#' className='flex item-center py-6 px-3 text-gray-700 hover:text-gray-600'>
-                            <svg className='h-6 w-6 mr-2 text-violet-800'xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
-</svg>
-                                <span className='font-extrabold text-3xl'>EmpowrHER</span>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className='flex items-center space-x-12'>
-                        <div><a href='#'>Home</a></div>
-                        <div><a href='#'>About Us</a></div>
-                        <div><a href='#'>Testimonials</a></div>
-                        <div><a href='#'>Team</a></div>
-                        <div>{user ? <SignOutButton2 /> : <SignInButton2 />}</div>
-                    </div>
-                </div>
+  return (
+    <nav className="bg-gradient-to-r from-violet-200 to-violet-100">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between">
+          <div className="flex space-x-4">
+            <div>
+              <a
+                href="#"
+                className="flex item-center py-6 px-3 text-gray-700 hover:text-gray-600"
+              >
+                <svg
+                  className="h-6 w-6 mr-2 text-violet-800"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
+                  />
+                </svg>
+                <span className="font-extrabold text-grey-800 text-3xl">
+                  Empowr{" "}
+                </span>{" "}
+                <span className="font-extrabold text-3xl text-violet-500">
+                  {" "}
+                  HER
+                </span>
+              </a>
             </div>
-        </nav>
-    )
+          </div>
+
+          <div className="flex items-center space-x-12">
+            <div>
+              <a href="#">Home</a>
+            </div>
+            <div>
+              <a href="#">About Us</a>
+            </div>
+            <div>
+              <a href="#">Testimonials</a>
+            </div>
+            <div>
+              <a href="#">Team</a>
+            </div>
+            <div>{user ? <SignOutButton2 /> : <SignInButton2 />}</div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
