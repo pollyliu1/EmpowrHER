bc5462eee5d03a28e165518e088d1549bb11e474
ricsign <ricsign123@gmail.com>
2023-01-21 01:19:30 -0500

    small change

--- client/src/components/Jumbotron.tsx
+++ client/src/components/Jumbotron.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import Navbar from './Navbar'
 
+import Navbar from './Navbar'
 import SignInButton from './SignInButton'
 import SignOutButton from './SignOutButton'
 
 function Jumbotron({ user }) {
   return (
     <>
+        <Navbar />
         {user ? 
         (<>
         {user.displayName}
         <SignOutButton />
-        </> ) : <SignInButton /> }
+        </> ) : 
+        <SignInButton /> }
     </>
   )
 }
