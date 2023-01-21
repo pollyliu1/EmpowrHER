5cfdfa25a167e06ff9a60db070f1cefb92a9bc5f
mkjshen <118473721+mkjshen@users.noreply.github.com>
2023-01-21 13:48:01 -0500

    Update posts.tsx
    added search bar to post page

--- client/src/pages/posts.tsx
+++ client/src/pages/posts.tsx
@@ -35,8 +35,23 @@ function Posts({ posts }) {
 
   return (
     <>
+    <div className="bg-[url('../../public/jumbotron-gradient.png')] bg-cover h-full">
       <Heading />
       <Navbar user={user} />
+      <div className='max-w-lg mx-auto mt-20 border-purple-800'>
+        <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden ">
+          <div className="grid place-items-center h-full w-12 text-gray-300">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          </div>
+          <input
+            className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+            type="text"
+            id="search"
+            placeholder="Search for a skill.." /> 
+        </div>
+      </div>
       <div>Posts</div>
       {/* {posts[0]["description"]} */}
       {posts.length > 0 &&
@@ -56,6 +71,7 @@ function Posts({ posts }) {
             </p>
           </div>
         ))}
+        </div>
     </>
   );
 }
