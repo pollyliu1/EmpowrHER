a956dac50a95ce3ef72d80b0873d0abe811ee390
mkjshen <118473721+mkjshen@users.noreply.github.com>
2023-01-21 02:06:35 -0500

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -11,19 +11,19 @@ function Navbar({ user }) {
                     <div className='flex space-x-4'>
                         <div>
                             <a href='#' className='flex item-center py-6 px-3 text-gray-700 hover:text-gray-600'>
-                            <svg className='h-6 w-6 mr-2 text-violet-400'xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <svg className='h-6 w-6 mr-2 text-violet-800'xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
 </svg>
-                                <span>EmpowrHER</span>
+                                <span className='font-extrabold text-3xl'>EmpowrHER</span>
                             </a>
                         </div>
                     </div>
 
                     <div className='flex items-center space-x-12'>
-                        <div><a>Home</a></div>
-                        <div><a>About Us</a></div>
-                        <div><a>Testimonials</a></div>
-                        <div><a>Team</a></div>
+                        <div><a href='#'>Home</a></div>
+                        <div><a href='#'>About Us</a></div>
+                        <div><a href='#'>Testimonials</a></div>
+                        <div><a href='#'>Team</a></div>
                         <div>{user ? <SignOutButton2 /> : <SignInButton2 />}</div>
                     </div>
                 </div>
