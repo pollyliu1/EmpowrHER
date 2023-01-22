a7861fb649134ca764c97413a681fc4e98193c38
mkjshen <118473721+mkjshen@users.noreply.github.com>
2023-01-21 19:27:38 -0500

    Update Footer.tsx
    removed text from footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,39 +2,7 @@ import React from "react";
 
 function Footer() {
     return (<section className="bg-white">
-    <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
-        <nav className="flex flex-wrap justify-center -mx-5 -my-2">
-            <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    About
-                </a>
-            </div>
-            <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    Blog
-                </a>
-            </div>
-            <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    Team
-                </a>
-            </div>
-            <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    Pricing
-                </a>
-            </div>
-            <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    Contact
-                </a>
-            </div>
-            <div className="px-5 py-2">
-                <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    Terms
-                </a>
-            </div>
-        </nav>
+        <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <div className="flex justify-center mt-8 space-x-6">
             <a href="#" className="text-gray-400 hover:text-gray-500">
                 <span className="sr-only">Facebook</span>
